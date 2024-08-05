e5c8fb650c377877421b13cf2b6f243521009c37
Ilyes CHELBI <ichelbi@leni.fr>
2024-08-05 09:52:36 +0100

    class instead of module

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const Glog = {}
+class Glog {
+  debug(message = "", data = {}) {
+    console.log('\x1b[36m%s\x1b[0m', "[CODE 100: SUCCESS]" + " " + message + " " + "was successfull 😉 Data => ", data);
+  }
 
-Glog.debug = function (message = "",data = {}) {
-    console.log('\x1b[36m%s\x1b[0m', "[CODE 100: SUCCESS]" +" "+ message+" "+ "was successfull 😉 Data => ",data);
-}
-
-Glog.clear_and_debug = function (message = "",data = {}) {
+  clear_and_debug(message = "", data = {}) {
     console.clear()
-    console.log('\x1b[36m%s\x1b[0m', "[CODE 100: SUCCESS]" +" "+ message+" "+ "was successfull 😉 Data => ",data);
-}
+    console.log('\x1b[36m%s\x1b[0m', "[CODE 100: SUCCESS]" + " " + message + " " + "was successfull 😉 Data => ", data);
+  }
 
-Glog.error = function (message = "",data = {}) {
-    console.log('\x1b[31m%s\x1b[0m', "[CODE 0: ERROR]" +" "+ message+" "+ "failed 😔 Data => ", data);
-}
+  error(message = "", data = {}) {
+    console.log('\x1b[31m%s\x1b[0m', "[CODE 0: ERROR]" + " " + message + " " + "failed 😔 Data => ", data);
+  }
 
-Glog.clear_and_showError = function(message="",data={}){
+  clear_and_showError(message = "", data = {}) {
     console.clear()
-    console.log('\x1b[31m%s\x1b[0m', "[CODE 0: ERROR]" +" "+ message+" "+ "failed 😔 Data => ", data);
+    console.log('\x1b[31m%s\x1b[0m', "[CODE 0: ERROR]" + " " + message + " " + "failed 😔 Data => ", data);
+  }
 }
 
 module.exports = Glog
