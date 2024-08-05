178f5b6d28ceeb96793e3d4911f6fb8de078ded8
Ilyes CHELBI <ichelbi@leni.fr>
2024-08-05 11:29:23 +0100

    adding types definitions

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module "Glog-types" {
+    interface Glog_method_type {
+      (message: string, data: object)
+    }
+   
+}
--- index.js
+++ index.js
@@ -1,21 +1,24 @@
- class Glog {
-   debug(message = "", data = {}) {
-     console.log('\x1b[36m%s\x1b[0m', "[CODE 100: SUCCESS]" + " " + message + " " + "was successfull 😉 Data => ", data);
-   }
+/// <reference path="./index.d.ts"/>
+// @ts-check
+class Glog {
+  /** @type {import('Glog-types').Glog_method_type} */
+  debug(message, data) {
+    console.log('\x1b[36m%s\x1b[0m', "[CODE 100: SUCCESS]" + " " + message + " " + "was successfull 😉 Data => ", data);
+  }
+  /** @type {import('Glog-types').Glog_method_type} */
+  clear_and_debug(message, data) {
+    console.clear()
+    console.log('\x1b[36m%s\x1b[0m', "[CODE 100: SUCCESS]" + " " + message + " " + "was successfull 😉 Data => ", data);
+  }
+  /** @type {import('Glog-types').Glog_method_type} */
+  error(message, data) {
+    console.log('\x1b[31m%s\x1b[0m', "[CODE 0: ERROR]" + " " + message + " " + "failed 😔 Data => ", data);
+  }
+  /** @type {import('Glog-types').Glog_method_type} */
+  clear_and_showError(message, data) {
+    console.clear()
+    console.log('\x1b[31m%s\x1b[0m', "[CODE 0: ERROR]" + " " + message + " " + "failed 😔 Data => ", data);
+  }
+}
 
-   clear_and_debug(message = "", data = {}) {
-     console.clear()
-     console.log('\x1b[36m%s\x1b[0m', "[CODE 100: SUCCESS]" + " " + message + " " + "was successfull 😉 Data => ", data);
-   }
-
-   error(message = "", data = {}) {
-     console.log('\x1b[31m%s\x1b[0m', "[CODE 0: ERROR]" + " " + message + " " + "failed 😔 Data => ", data);
-   }
-
-   clear_and_showError(message = "", data = {}) {
-     console.clear()
-     console.log('\x1b[31m%s\x1b[0m', "[CODE 0: ERROR]" + " " + message + " " + "failed 😔 Data => ", data);
-   }
- }
-
- module.exports = Glog
+module.exports = Glog
