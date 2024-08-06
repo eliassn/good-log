1b615bf709599f6a6e3b40c471dce15d0db31eaa
Ilyes CHELBI <ichelbi@leni.fr>
2024-08-06 07:56:01 +0100

    meth type void

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
 declare module "Glog-types" {
     type debug = {
-      (message: string, data: object)
+      (message: string, data: object) : void
     }
     type clear_and_debug = {
-      (message: string, data: object)
+      (message: string, data: object) : void
     }
     type error ={
-      (message: string, data: object)
+      (message: string, data: object) : void
     }
     type clear_and_showError = {
-      (message: string, data: object)
+      (message: string, data: object) : void
     }
 }
